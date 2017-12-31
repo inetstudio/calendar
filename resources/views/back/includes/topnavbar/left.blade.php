f07e90917b7e9287b6f31a4c21caab7870363afb
Konstantin Bukin <bukin@inetstudio.ru>
2017-12-31 15:09:09 +0300

    Includes views

---
+++ resources/views/back/includes/topnavbar/left.blade.php
@@ -0,0 +1,3 @@
+<li class="{{ isActiveRoute('back.calendar.*') }}">
+    <a href="{{ route('back.calendar.index') }}"><i class="fa fa-lg fa-calendar"></i> <span class="nav-label">Календарь публикаций</span></a>
+</li>
